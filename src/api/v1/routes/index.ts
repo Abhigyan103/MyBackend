0a886ae58c21b8cb3467433e68f10425d48f58e0
Abhigyan Singh <abhigyansingh56@gmail.com>
2025-10-09 01:19:51 +0530

    fix: Minor fixes

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
-import authRoutes from './auth.routes.js';
+import { Router } from "express";
+import authRoutes from "./auth.routes.js";
+import postRoutes from "./post.routes.js";
+import { protectFor } from "../../../middleware/auth.middleware.js";
+import { Role } from "../../../types/roles.js";
 
 const router = Router();
 
-router.use('/auth', authRoutes);
+router.use("/auth", authRoutes);
+router.use("/posts", protectFor([Role.ADMIN]), postRoutes);
 
 export default router;
